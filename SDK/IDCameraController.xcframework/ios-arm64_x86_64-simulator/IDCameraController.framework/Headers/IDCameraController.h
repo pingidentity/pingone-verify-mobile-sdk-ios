3c47618145839ccefbdbd8d47d2b16e6341854aa
Abhishek Mahuri <amahuri@pingIdentity.com>
2024-11-04 17:00:14 +0530

    Staging feature release: 1. IDRND changes 2. Retry for Govt id and selfie 3. Geolocation 4. Ping Id bug fixes

---
+++ SDK/IDCameraController.xcframework/ios-arm64_x86_64-simulator/IDCameraController.framework/Headers/IDCameraController.h
@@ -0,0 +1,17 @@
+//
+//  IDCameraController.h
+//  IDCameraController
+//
+//  Created by idrnd on 5/14/20.
+//  Copyright © 2020 idrnd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDCameraController.
+FOUNDATION_EXPORT double IDCameraControllerVersionNumber;
+
+//! Project version string for IDCameraController.
+FOUNDATION_EXPORT const unsigned char IDCameraControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDCameraController/PublicHeader.h>
