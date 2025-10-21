96472110726309449357b20db94cccb125a13121
kdevipriya55 <kdevipriya@pingidentity.com>
2025-10-21 21:29:23 +0530

    PingOneVerify SDK v2.3.0

---
+++ SDK/Common/JoseTools.xcframework/ios-arm64_x86_64-simulator/JoseTools.framework/Headers/JoseTools.h
@@ -0,0 +1,18 @@
+//
+//  JoseTools.h
+//  JoseTools
+//
+//  Created by Bhavya Chauhan on 3/17/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JoseTools.
+FOUNDATION_EXPORT double JoseToolsVersionNumber;
+
+//! Project version string for JoseTools.
+FOUNDATION_EXPORT const unsigned char JoseToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JoseTools/PublicHeader.h>
+
+
---
+++ SDK/Verify/LanguagePackProvider.xcframework/ios-arm64/LanguagePackProvider.framework/Headers/LanguagePackProvider.h
@@ -0,0 +1,18 @@
+//
+//  LanguagePackProvider.h
+//  LanguagePackProvider
+//
+//  Created by Abhishek Mahuri on 19/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LanguagePackProvider.
+FOUNDATION_EXPORT double LanguagePackProviderVersionNumber;
+
+//! Project version string for LanguagePackProvider.
+FOUNDATION_EXPORT const unsigned char LanguagePackProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanguagePackProvider/PublicHeader.h>
+
+
