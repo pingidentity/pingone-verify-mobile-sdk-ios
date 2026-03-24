ab654cd14e51458658507a73e08c1d22a015e966
kdevipriya55 <kdevipriya@pingidentity.com>
2026-03-24 11:05:12 +0530

    Updated the Verify frameworks for 3.2.0

---
+++ Sample Code/PingOne Verify/Verify/PingOneVerify.xcframework/ios-arm64_x86_64-simulator/PingOneVerify.framework/Headers/PingOneVerify.h
@@ -0,0 +1,18 @@
+//
+//  PingOneVerify.h
+//  PingOneVerify
+//
+//  Created by Bhavya Chauhan on 10/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PingOneVerify.
+FOUNDATION_EXPORT double PingOneVerifyVersionNumber;
+
+//! Project version string for PingOneVerify.
+FOUNDATION_EXPORT const unsigned char PingOneVerifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingOneVerify/PublicHeader.h>
+
+
